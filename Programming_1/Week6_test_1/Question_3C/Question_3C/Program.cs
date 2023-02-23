835401e344e2e0976f7a40d0195f16e75c027f4c
Ujjwal Poudel <ujjwal.poudel.2003@gmail.com>
2023-02-22 22:46:24 -0500

    added week 3, software engineering fundament assignment part A

---
+++ Programming_1/Week6_test_1/Question_3C/Question_3C/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Question_3C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ====================      Test #1      ====================
+             * 
+             * Name: Ujjwal Poudel
+             * Student ID: 301284284
+             *
+             * Question 3c.
+             * 6 Marks.
+             *
+             * Write a program that prompts the user for a number. The 
+             * program prints the number and the letter equivalent. The 
+             * letter equivalent is obtained by casting the double to a 
+             * char. You must match the format exactly.
+             * 
+             * YOU MUST READ IN THE INPUT AS A double and you must do an explicit cast
+             * [For testing purposes, use a number in the range 48 to 90]
+             */
+
+            Console.Write("Enter the value as double: ");
+            double value = Convert.ToDouble(Console.ReadLine());
+            char character = (char) value;
+            Console.WriteLine($"The number {value} corresponds to letter {character}");
+        }
+    }
+}
---
+++ Programming_1/Week6_test_1/Question_5A/Question_5A/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Question_5A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ====================      Test #1      ====================
+             * 
+             * Name: Ujjwal Poudel
+             * Student ID: 301284284
+             *
+             * Question 5A.
+             * 12 Marks.
+             *
+             * TTC has asked you to Write a program that prompts the user 
+             * for the category of rider. Based on the user's  input the 
+             * program will display the following:
+             * 
+             * Input: student
+             * Message: student pays $1.75
+             * 
+             * Input: ADULT
+             * Message: ADULT pays $3.75
+             * 
+             * Input: Senior
+             * Message: Senior pays $2.00
+             * 
+             * any other input
+             * Message: I am sorry I don't know of that class of riders
+             * 
+             * You may use either the ToUpper() or ToLower() methods 
+             * 
+             * YOU MUST USE ONLY A SWITCH statement to solve this problem
+             * [For full marks you need to accept all combinations of 
+             * student, adult and senior]
+             */
+
+            Console.Write("What is your rider category: ");
+            string category = Console.ReadLine().ToLower();
+
+            switch (category)
+            {
+                case "student":
+                    Console.WriteLine("Student Pays $1.75");
+                    break;
+
+                case "adult":
+                    Console.WriteLine("ADULT Pays $2.00");
+                    break;
+
+                case "senior":
+                    Console.WriteLine("Senior pays $2.00");
+                    break;
+
+                default:
+                    Console.WriteLine("I am sorry I don't know of that class of riders");
+                    break;
+            }
+        }
+    }
+}
