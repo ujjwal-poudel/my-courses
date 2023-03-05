fa191480acbfa862367c0547fa2c846640994cf5
Ujjwal Poudel <ujjwal.poudel.2003@gmail.com>
2023-03-05 13:06:18 -0500

    add week 7 loop assignments

---
+++ Programming_1/Week_7_assignment/for_while_loops/Q11/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+/*
+ * Print the decimal, octal, and hexadecimal values of all characters between the start and stop
+ * characters entered by a user. For example, if the user enters an a and a z, the program should
+ * print all the characters between a and z and their respective numerical values. Make sure that the
+ * second character entered by the user occurs later in the alphabet than the first character. If it does
+ * not, write a loop that repeatedly asks the user for a valid second character until one is entered.
+ * Your output should be formatted as shown below
+ * Letter Decimal  Octal  Hex
+ *   a      97      141   61
+ *   b      98      142   62
+ *   c      99      143   63
+ *   d      100     144   64
+ *   e      101     145   65
+ * to obtain an int from a char use an explicit cast
+ * to obtain an octal from an int use Convert.ToString(«number_value», 8)
+ * to obtain a hexadecimal from an int use Convert.ToString(«number_value», 16)
+ */
+
+namespace Q11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char start, end;
+            //while loop
+            while (true)
+            {
+                Console.Write("Enter starting character from a to z: ");
+                start = Convert.ToChar(Console.ReadLine());
+                Console.Write("Enter ending character from a to z: ");
+                end = Convert.ToChar(Console.ReadLine());
+
+                if (end > start)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Letter Decimal Octal Hex");
+            while (start<= end)
+            {
+                int decimalValue = (int)start;
+                string octalValue = Convert.ToString(decimalValue, 8);
+                string hexValue = Convert.ToString(decimalValue, 16);
+                Console.WriteLine($"{start,3}{decimalValue,9}{octalValue,7}{hexValue,4}");
+                start++;
+            }
+
+            ////for loop
+            //for (;;)
+            //{
+            //    Console.Write("Enter the first letter in alphabet: ");
+            //    start = Convert.ToChar(Console.ReadLine());
+
+            //    Console.Write("Enter the second letter in alphabet: ");
+            //    end = Convert.ToChar(Console.ReadLine());
+
+            //    if (end > start)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine("Letter Decimal Octal Hex");
+            //for (;start <= end;)
+            //{
+            //    int decimalValue = (int)start;
+            //    string octalValue = Convert.ToString(decimalValue, 8);
+            //    string hexValue = Convert.ToString(decimalValue, 16);
+            //    Console.WriteLine($"{start, 3}{decimalValue, 9}{octalValue, 7}{hexValue, 4}");
+            //    start++;
+            //}
+        }
+    }
+}
---
+++ Programming_1/Week_7_assignment/for_while_loops/Q9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+/*
+ * Write and run a C# program that calculates and displays the amount of money available in a bank
+ * account that initially has $1,000 deposited in it and that earns 8 percent interest a year. Your
+ * program should display the amount available at the end of each year for a period of ten years. Use
+ * the relationship that the money available at the end of each year equals the amount of money in
+ * the account at the start of the year plus .08 times the amount available at the start of the year.
+ * Your output should be formatted as shown below
+ * Year Balance
+ * 1 $1,080.00
+ * 2 $1,166.40
+ * 3 $1,259.71
+ * 4 $1,360.49
+ * 5 $1,469.33
+ * 6 $1,586.87
+ * 7 $1,713.82
+ * 8 $1,850.93
+ * 9 $1,999.00
+ * 10 $2,158.92
+ */
+
+namespace Q9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialBalance = 1000;
+            double interest = 0.08;
+            double balance;
+            int year = 1;
+
+            ////while loop
+            //Console.WriteLine( "Year    Balance");
+            //Console.WriteLine("-----------------");
+            //while(year <=10)
+            //{
+            //    balance = initialBalance + (interest * initialBalance);
+            //    Console.WriteLine($"{year,1}{balance,16:C}");
+            //    initialBalance = balance;
+            //    year++;
+            //}
+
+            //for loop
+            for(;year <=10; year++)
+            {
+                balance = initialBalance + (interest * initialBalance);
+                Console.WriteLine($"{year,1}{balance,16:C}");
+                initialBalance = balance;
+            }
+        }
+    }
+}
